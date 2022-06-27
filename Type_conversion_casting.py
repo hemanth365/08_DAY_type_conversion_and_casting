7feaf99e85e6db80a825f7d4a55c6baa8e1210ae
Hemanth Krishna Nanda <107603676+hemanth365@users.noreply.github.com>
2022-06-27 18:49:59 +0530

    Add files via upload

---
+++ Type_conversion_casting.py
@@ -0,0 +1,36 @@
+# a=6 ; b='python' ; c='terminal'
+# print(a+b+c)    # TYPE ERROR
+
+# a=6 ; b='python' ; c='terminal'
+# print(str(a)+b+c)  #6pythonterminal
+
+# a=6 ; b='python' ; c='terminal'
+# print(b+str(a)+c)    #python6terminal
+
+# a=6 ; b='python' ; c='terminal'
+# print(c +'_'+str(a)+b)    #terminal_python6
+
+# a=6 ; b='python' ; c='terminal'
+# print(b+'-'+str(a)+c)    #python-6terminal
+
+
+'''x=25
+print(str(x))      #x=25
+print(float(x))    #x=25.0
+
+Y=33.8
+print(str(Y))       #Y=33.8
+print(int(Y))       #Y=33
+
+A='HAPPY' ; B='BIRTHDAY'
+print(A+'-'+B)'''       # HAPPY-BIRTHDAY
+
+
+
+
+
+
+
+
+
+
